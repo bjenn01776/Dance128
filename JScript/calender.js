72beaca93bb4a614f1a6b85a538a3e726084a7e3
bjenn01776 <bjenn1776@verizon.net>
2025-08-27 10:33:58 -0400

    Add files via upload

--- JScript/calender.js
+++ JScript/calender.js
@@ -1,92 +1,108 @@
 
 function calender() {
+
     console.log(" in calender()");
 
-    const arrMonth = ["January", "February", "March", "April", "MAy", "June", "July", "August", "September", "October", "November", "Decemeber"];
+    const arrMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Decemeber"];
+    var arrMonNDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const arrDay   = ["Sunday", "Monday", "Tuesday", "Wedsneday", "Thursday", "Friday", "Saturday"];
 
-    const arrDay = ["Sunday", "Monday", "Tuesday", "Wedsneday", "Thursday", "Friday", "Saturday"];
-
-    let dt = new Date();
-    // let dt = new Date("Novemberr,1, 2025");
-
-    console.log("Today's date is::  " + dt);
-
-    let mon = dt.getMonth();
-    console.log("The month is ::  " + mon);
-
-    let monthName = arrMonth[dt.getMonth()]
-
+    let date = new Date();
+    console.log("The date is::  " + date);
+    date.setDate(1);
+    var month = date.getMonth();
+    console.log("The month is ::  " + month);
+    let monthName = arrMonth[date.getMonth()]
     console.log("The month is ::  " + monthName)
-
-    let monprt = null;
-    try {
-        monprt = document.getElementsByClassName('month');
-        // monprt.style.background_color = "gray";
-        monprt.textContent = "TEWTER";
-        monprt.style.borderwidth = "10px";
-
-    }
-    catch (e) {
-        console.log(e);
-    }
-
-
-
-
-    let h1Tag = document.querySelector("#mname")
-    h1Tag.innerText = monthName;
-
-
-    dt.setDate(2025, dt.getMonth() + 1, 1);
-    let date = new Date(2025, mon, 1)
     dow = date.getDay()
     console.log("The first of the month is on::  " + date.getDay())
     console.log("The first of the month is on::  " + arrDay[date.getDay()])
 
-    let newDiv
-    let newH1
-    let bd = document.getElementById('cal');
+    let h1_Title = document.querySelector("#mname")
+    h1_Title.innerText = monthName;
+
+ 
+    let nMonDays = arrMonNDay[month];
+    buildMonth(dow, nMonDays);
+ 
 
 
+    let refBtn = document.getElementById("bup");
+    refBtn.addEventListener("click", (evt) => {
+        try {
+            console.log(evt.target.textContent);
+            month++;
+            date = new Date(2025, month, 1);
+            h1_Title.textContent = arrMonth[month] + "  😜";
+            dow = date.getDay();
+            nMonDays = arrMonNDay[month];
+            buildMonth(dow, nMonDays);
+        }
+        catch (error) {
+            console.log(err);
+        }
+    })
 
+    let refBtnDwn = document.getElementById("bdwn");
+    refBtnDwn.addEventListener("click", (evt) => {
+        try {
+            console.log(evt.target.textContent);
+            month--;
+            date = new Date(2025, month, 1);
+            h1_Title.textContent = arrMonth[month] + "  🤔";
+            dow = date.getDay()
+            nMonDays = arrMonNDay[month];
+            buildMonth(dow, nMonDays);
+        }
+        catch (error) {
+            console.log(err);
+        }
+    })
+}
+
+
+let buildMonth = function (dow, nMonDays) {
+
+    let calRef = document.getElementById('cal')
+
+
+    let lst_blkDay = document.querySelectorAll('#cal .boxND');
+    for (k = 0; k < lst_blkDay.length; k++) {
+        lst_blkDay[k].remove();
+    }
+
+    let lstHTML = document.querySelectorAll('#cal .box');
+    for (k = 0; k < lstHTML.length; k++) {
+        lstHTML[k].remove();
+    }
+
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("boxND");
     for (j = 0; j < dow; j++) {
         newDiv = document.createElement('div');
         newDiv.className = "boxND";
-        newH1 = document.createElement('h1');
-        // newH1.innerText = parseInt(k);;
-        newDiv.appendChild(newH1);
-
-        //console.log(bd.innerText);
-        bd.appendChild(newDiv);
+        calRef.appendChild(newDiv);
     }
 
-    for (k = 1; k < 32; k++) {
+    for (k = 0; k < (nMonDays); k++) {
         newDiv = document.createElement('div');
         newDiv.className = "box";
         newH1 = document.createElement('h1');
-        newH1.innerText = parseInt(k);;
+        newH1.innerText = parseInt(k + 1);;
         newDiv.appendChild(newH1);
+        calRef.appendChild(newDiv);
 
-        // console.log(bd.innerText);
-        bd.append(newDiv);
-    }
+        newDiv.addEventListener("click", (e) => {
+            try {
+                console.log("Day click event");
+                console.log(e.target.textContent + " " + nMonDays );
+            }
+            catch (e) {
+                console.log(e);
+            }
+        });
 
-
-
-
-    let listBox = document.getElementsByClassName("box");
-    console.log(typeof (listBox));
-
-    h1Tag.addEventListener("mouseover", event => { h1Tag.textContent = "mouver " + "😜" });
-    h1Tag.addEventListener("mouseout", event => { h1Tag.textContent = "mouy "+ "🤔" });
-
-    for (kk = 1; kk < listBox.length; kk++) {
-        listBox[kk].addEventListener("click", (e) => {
-            console.log("event");
-            console.log(listBox[kk].textContent)
-        }
-        );
 
     }
+
 }
-
