30309d760b80af1a747ae0a122bfa266f81924d7
bjenn01776 <bjenn1776@verizon.net>
2025-12-18 10:53:05 -0500

    Add files via upload
    Fixed Calendar - Days are active now

--- JScript/calender.js
+++ JScript/calender.js
@@ -1,43 +1,69 @@
 
 
-const arrMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const arrMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var arrMonNDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     const arrDay   = ["Sunday", "Monday", "Tuesday", "Wedsneday", "Thursday", "Friday", "Saturday"];
-     const arrDay2   = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const arrDay2  = ["Sun",    "Mon",    "Tue",     "Wed",       "Thu",      "Fri",    "Sat"];
 
+   
+
+const calDay = { year: 1955, month: 9, day: 11 };
 
 function calender(e) {
 
-    console.log(" in calender()");
+    console.log(" In calender().js");
 
-    
     let date = new Date();
     console.log("The date is::  " + date);
-    date.setDate(1);
-    var month = date.getMonth();
-    console.log("The month is ::  " + month);
+    year = date.getFullYear();
+    console.log("The year is::  " + year);  
+
+    ////////////////////////////////
+    ///  beginning of the month  ///
+    ////////////////////////////////
+    date.setDate(1);  
+    var month     = date.getMonth();
     let monthName = arrMonth[date.getMonth()]
-    console.log("The month is ::  " + monthName)
-    dow = date.getDay()
-    console.log("The first of the month is on::  " + date.getDay())
-    console.log("The first of the month is on::  " + arrDay[date.getDay()])
+    dow           = date.getDay();
+
+    calDay.month = month;
+    calDay.year  = year;
+    
+    console.log("   The 1st month    number:: " + month);
+    console.log("   The 1st month     name :: " + monthName); 
+    console.log(`   The 1st day of the week::  ${dow}`)
+    console.log("   The first of the month is on::  " + arrDay[date.getDay()])
+    
+    
 
     let h1_Title = document.querySelector("#mname")
     h1_Title.innerText = monthName;
 
- 
     let nMonDays = arrMonNDay[month];
     buildMonth(dow, nMonDays);
  
 
-    let refBtn = document.getElementById("bup");
+    let refBtn = document.getElementById("bdwn");
     refBtn.addEventListener("click", (evt) => {
         try {
             console.log(evt.target.textContent);
             month++;
-            date = new Date(2025, month, 1);
+            if (month > 11) { 
+                month = 0;
+                year++;
+                calDay.month = month;
+                calDay.year  = year;
+            }    
+
+            date = new Date(year, month, 1);
+             
+            console.log("   New  Date:: " + date);
+            console.log("   New Month:: " + month);
+            console.log("   New  Year:: " + year);
+
             h1_Title.textContent = arrMonth[month] + "  😜";
-            dow = date.getDay();
+
+            dow      = date.getDay();
             nMonDays = arrMonNDay[month];
             buildMonth(dow, nMonDays);
         }
@@ -46,12 +72,19 @@ function calender(e) {
         }
     })
 
-    let refBtnDwn = document.getElementById("bdwn");
+    let refBtnDwn = document.getElementById("bup");
     refBtnDwn.addEventListener("click", (evt) => {
         try {
             console.log(evt.target.textContent);
-            month--;
-            date = new Date(2025, month, 1);
+            if( month == 0){
+                month = 11;
+                year--;
+            } 
+            else month--;
+            
+            calDay.month = month;
+            calDay.year  = year;
+            date = new Date(year, month, 1);
             h1_Title.textContent = arrMonth[month] + "  🤔";
             dow = date.getDay()
             nMonDays = arrMonNDay[month];
@@ -64,11 +97,13 @@ function calender(e) {
 }
 
 
-let buildMonth = function (dow, nMonDays) {
+
+let buildMonth = function (dow, nMonthDays) {
 
     let calRef = document.getElementById('cal')
 
-
+    /////////////////////////
+    /// Remove existing days
     let lst_blkDay = document.querySelectorAll('#cal .boxND');
     for (k = 0; k < lst_blkDay.length; k++) {
         lst_blkDay[k].remove();
@@ -78,18 +113,32 @@ let buildMonth = function (dow, nMonDays) {
     for (k = 0; k < lstHTML.length; k++) {
         lstHTML[k].remove();
     }
+    /// End Remove existing days
+    ////////////////////
+    /////////////////////////
 
+
+    ///First Row - Blank Days
     let newDiv = document.createElement("div");
     newDiv.classList.add("boxND");
     for (j = 0; j < dow; j++) {
         newDiv = document.createElement('div');
         newDiv.className = "boxND";
+        // newDiv.style.backgroundColor = "pink";
         calRef.appendChild(newDiv);
     }
 
-    for (k = 0; k < (nMonDays); k++) {
+
+    for (k = 0; k < (nMonthDays); k++) {
         newDiv = document.createElement('div');
         newDiv.className = "box";
+        var  xyear = calDay.year;
+        var xmonth = calDay.month;
+        calDate= new Date(xyear,xmonth,k+1);
+        calDOW = calDate.getDay();  
+        if (calDOW > 0  &&  calDOW < 5) {
+            newDiv.style.backgroundColor = "lightpink";
+        }
         newH1 = document.createElement('h1');
         newH1.innerText = parseInt(k + 1);;
         newDiv.appendChild(newH1);
@@ -97,40 +146,26 @@ let buildMonth = function (dow, nMonDays) {
 
 
         newDiv.addEventListener("click", (e) => {
+
             try {
-                console.log("Day click event");
-                console.log(e.target.textContent + " " + nMonDays );
-                let date = new Date();
-                console.log("The date is::  " + date); 
+                console.log("\n\n\n *** Day Click Event *** ");
+                
+                var month = calDay.month;
+                var year  = calDay.year;
+                console.log("    The Cal month  # :: " + calDay.month);
+                console.log("    The Cal month is :: " + arrMonth[calDay.month]);
+                console.log("    The Cal   DOM is :: " + e.target.textContent);
 
-                var month = date.getMonth(); 
-            
-                let monthName = arrMonth[date.getMonth()]
-                MonthName = monthName.toUpperCase();
-                console.log("The month is ::  " + MonthName);
-               
-                let day = date.getDay();
-                console.log("The day is ::  " + day);
-              //  console.log("The first of the month is on::  " + date.getDay())
-              //  console.log("The first of the month is on::  " + arrDay[date.getDay()])
-
-
-                console.log("DOM: " + e.target.textContent);
-                let dateDOM = new Date(2025, month, e.target.textContent);
+                let dateDOM = new Date( year, month, e.target.textContent );
+                console.log("    The Cal   date is::  " + dateDOM); 
                 let dayName = arrDay2[dateDOM.getDay()];
-                console.log("Date Object: " + dateDOM);
-                console.log("Date Name: " + dayName);
-
-               
+                console.log("    The Cal  Day Name:: " + dayName);
 
 
-                var filen = "../JAVA/dSched_" + MonthName + "_" + e.target.textContent + "_" + dayName + ".html" ;
+                var filen = "../CalDays/dSched_" + arrMonth[month] + "_" + e.target.textContent + "_" + dayName + ".html" ;
+                console.log("File Name is :: "  + filen);
 
-                 console.log("File Name is ::  " + filen);
-
-                // window.location.href = "../JAVA/dSched_DECEMBER_18_Sat.html";
-                   window.location.href = filen;
-
+                window.location.href = filen;
 
             }
             catch (e) {
@@ -139,12 +174,9 @@ let buildMonth = function (dow, nMonDays) {
         });
 
     }
- 
-
-
-
-
- 
 
         
-    }
+}
+
+
+ 
